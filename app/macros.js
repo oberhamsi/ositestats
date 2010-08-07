20df20ed92acbc2a09dc9c9eb063652e4ca7d132
Simon Oberhammer <simon@nekapuzer.at>
2010-08-07 13:45:58 +0200

    minor stats layout improv

--- app/macros.js
+++ app/macros.js
@@ -1,7 +1,12 @@
 var {clickGraphSettings} = require('./config');
+var {keyToDate} = require('./model');
 
 exports.clickGraphImgUrl_macro = function(tag) {
 	var site = tag.parameters[0];
 	var month = tag.parameters[1];
 	return clickGraphSettings.url + site + '_' + month + '.png';
+};
+
+exports.keyToDate_filter = function(input) {
+   return keyToDate(String(input));
 };
