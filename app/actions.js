ff2bdf8ce6fcbfc92ffb20818767d1f28b40bafd
Simon Oberhammer <simon@nekapuzer.at>
2010-09-29 11:00:52 +0200

    fixes content-type and content of hit/blank actions

--- app/actions.js
+++ app/actions.js
@@ -48,6 +48,7 @@ exports.hit = function(req) {
    var redirectResponse = new Response('See other: /blank');
    redirectResponse.status = 302;
    redirectResponse.setHeader('Location', '/blank');
+   redirectResponse.setHeader('Content-Type', 'application/x-javascript');
    
    var ip = req.remoteAddress;
    var forwardedFor = req.getHeader("X-Forwarded-For");
@@ -86,8 +87,8 @@ exports.hit = function(req) {
 exports.blank = function(req) {
    return {
       status: 200, 
-      headers: {'Content-Type': 'image/gif'},
-      body: [new ByteString([71,73,70,56,57,97,1,0,1,0,128,0,0,255,255,255,255,255,255,33,249,4,1,10,0,1,0,44,0,0,0,0,1,0,1,0,0,2,2,76,1,0,59])],
+      headers: {'Content-Type': 'applicaton/x-javascript'},
+      body: ['{}'],
    };
 }
 
